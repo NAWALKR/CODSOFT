d14e0adb2f08eb378cc340a3c55ac9d58d907c63
Nawal Kishor Yadav <129495960+NAWALKR@users.noreply.github.com>
2023-09-10 20:47:42 +0530

    Add files via upload

---
+++ ATM INTERFACE.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
+    }
+}
+
+class ATM {
+    private BankAccount account;
+    private Scanner scanner;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+        scanner = new Scanner(System.in);
+    }
+
+    public void showOptions() {
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+    }
+
+    public void run() {
+        int choice;
+        do {
+            showOptions();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 4);
+    }
+
+    private void checkBalance() {
+        System.out.println("Your balance is: " + account.getBalance());
+    }
+
+    private void deposit() {
+        System.out.print("Enter deposit amount: ");
+        double amount = scanner.nextDouble();
+        account.deposit(amount);
+    }
+
+    private void withdraw() {
+        System.out.print("Enter withdrawal amount: ");
+        double amount = scanner.nextDouble();
+        account.withdraw(amount);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000.0); // Initial balance of $1000
+        ATM atm = new ATM(userAccount);
+        atm.run();
+    }
+}
---
+++ STUDENT GRADING SYSTEM.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = input.nextInt();
+        
+        int[] marks = new int[numSubjects];
+        
+        // Input marks for each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = input.nextInt();
+        }
+        
+        // Calculate total marks
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
+        
+        // Assign grades based on average percentage
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        
+        // Display results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        input.close();
+    }
+}
